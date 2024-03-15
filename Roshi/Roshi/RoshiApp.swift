624993ff440f81f10036877fea1acaaad6273f43
Pablo Salas <pablosalas_@outlook.com>
2024-03-15 16:53:05 -0600

    Sofia de chocolate

---
+++ Roshi/Roshi/RoshiApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoshiApp.swift
+//  Roshi
+//
+//  Created by Pablo Salas on 15/03/24.
+//
+
+import SwiftUI
+
+@main
+struct RoshiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
